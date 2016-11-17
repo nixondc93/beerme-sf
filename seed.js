f07703fda2d1c9b1dbf0f8a7783eec9e01399ba2
ladydangerdame <turbofemme@gmail.com>
2016-11-17 11:35:49 -0800

    Added seed data

---
+++ seed.js
@@ -0,0 +1,49 @@
+var db = require("./models");
+
+var beerList = [{
+    name: "Super Beer",
+    description: "The most delicious beer",
+    rating: 4,
+    Brewery: "Your Mom's Brewery",
+    abv: "8.6%"
+}, {
+    name: "Mega Beer",
+    description: "The most delicious beer",
+    rating: 5,
+    Brewery: "Your Dad's Brewery",
+    abv: "9.6%"
+}, {
+    name: "Mondo Beer",
+    description: "The most delicious beer",
+    rating: 5,
+    Brewery: "Your Sister's Brewery",
+    abv: "9.8%"
+}, {
+    name: "Ultra Beer",
+    description: "The most delicious beer",
+    rating: 5,
+    Brewery: "Your Brother's Brewery",
+    abv: "8.6%"
+}];
+
+var locationList = [{
+    Name: "Beer Store",
+    TypeOf: "Liquor Store",
+    Address: "123 place place place SF",
+    phone: "123-456-7890"
+}, {
+    Name: "Beer Stop",
+    TypeOf: "Corner Store",
+    Address: "123 place place place SF",
+    phone: "123-456-7890"
+}, {
+    Name: "Beer Mall",
+    TypeOf: "Liquor Store",
+    Address: "123 place place place SF",
+    phone: "123-456-7890"
+}, {
+    Name: "Beer 'n Things'",
+    TypeOf: "Corner Store",
+    Address: "123 place place place SF",
+    phone: "123-456-7890"
+}];
