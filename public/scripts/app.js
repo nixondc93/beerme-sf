ecd52e94f11f644061fd097d7afc597160fab20f
Derry Nixon <nixondc93@gmail.com>
2016-11-17 22:08:06 -0800

    handlebars templating for locations complete

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,27 +14,47 @@ $(document).ready(function(){
 			success: getBeersSucc,
 			error: getBeersErr
 		});
+    $.ajax({
+        method: 'GET',
+        url: '/api/location',
+        success: getlocationsSucc,
+        error: getlocationsErr
+      });
+
 
 
 });///////////end of document.ready ////////////////////////
 
-
+///////////get beers ajax functions/////////////////
 function getBeersErr(err){
   console.error(error);
 }
 
 function getBeersSucc(json){
   json.forEach(function(beer) {
-      renderBeer(beer);
+      render(beer, $('#beer-template'), $('#beers'));
+  });
+}
+
+
+///////////get locations ajax functions///////////////
+
+function getlocationsErr(err){
+  console.error(error);
+}
+
+function getlocationsSucc(json){
+  json.forEach(function(locations) {
+      render(locations, $('#location-template'), $('.location-target'));
   });
 }
 
 
 
- function renderBeer(beer) {
-   console.log('rendering beer', beer);
-   var beerHtml = $('#beer-template').html();
-   var beerTemplate = Handlebars.compile(beerHtml);
-   var html = beerTemplate(beer);
-   $('#beers').prepend(html);
+///////////hb template rendering/////////////
+ function render(json, html, target) {
+   console.log('rendering', json);
+   var hbTemplate = Handlebars.compile(html.html());
+   var html = hbTemplate(json);
+   target.prepend(html);
  }
