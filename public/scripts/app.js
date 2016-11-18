f149db38d9c86fc1a4913432762a78bff8029543
ladydangerdame <turbofemme@gmail.com>
2016-11-17 19:37:00 -0800

    App.js edit

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,26 @@
-$(document).ready(function(){
-  console.log('sanity check');
-   $('.carousel').carousel();
+$(document).ready(function() {
+    console.log('sanity check');
+    $('.carousel').carousel();
 
-   var brewerydb_api = "http://api.brewerydb.com/v2/?key=6150ca217ac01919daaaea494e109607"
-
-
+    var brewerydb_api = "http://api.brewerydb.com/v2/?key=6150ca217ac01919daaaea494e109607"
 
 
- });
+    // submit event on form
+    $('form').on('submit', function(event) {
+        $.ajax({
+            method: 'GET',
+            url: endpoint,
+            // The data to send aka query parameters
+            data: $("form").serialize(),
+            dataType: 'json',
+            success: onSubmitReqSuccess
+        });
+    });
+
+    function onSubmitReqSuccess(json) {
+        console.log(json);
+        // process data
+    }
+
+
+});
